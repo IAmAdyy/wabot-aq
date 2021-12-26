448f2c580e08dc292b451fca3f903ca98dd40007
buf = await downloadM() <82200440+Adiixyz@users.noreply.github.com>
2021-12-26 19:52:14 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,6 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [085895086040]
-│ • Telkomsel [081334177340]
-╰────
-
-╭─「 Donasi • Non Pulsa 」
-│ • https://saweria.co/Nurutomo
-│ • https://saweria.co/ariffb
+│ • 60199782326
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
